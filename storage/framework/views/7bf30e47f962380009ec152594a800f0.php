9a2f5c2f32cec303c6cf8b5a22f90592f873caf1
taranjot singh <taranjot.singh@geeky.dev>
2024-12-05 16:36:27 +0530

    initial commit

---
+++ storage/framework/views/7bf30e47f962380009ec152594a800f0.php
@@ -0,0 +1,8 @@
+<p class="price-label text-sm text-zinc-500 max-sm:text-xs">
+    <?php echo app('translator')->get('shop::app.products.prices.configurable.as-low-as'); ?>
+</p>
+
+<p class="final-price font-semibold">
+    <?php echo e($prices['regular']['formatted_price']); ?>
+
+</p><?php /**PATH /var/www/html/emart/packages/Webkul/Shop/src/Providers/../Resources/views/products/prices/configurable.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b81135b969e44f1a70153f0c519c1a58.php
@@ -0,0 +1,8 @@
+<p class="price-label text-sm text-zinc-500 max-sm:leading-4">
+    <?php echo app('translator')->get('shop::app.products.prices.grouped.starting-at'); ?>
+</p>
+
+<p class="font-semibold max-sm:leading-4">
+    <?php echo e($prices['final']['formatted_price']); ?>
+
+</p><?php /**PATH /var/www/html/emart/packages/Webkul/Shop/src/Providers/../Resources/views/products/prices/grouped.blade.php ENDPATH**/ ?>
